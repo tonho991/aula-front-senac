e84620d03a8fc1499514fc8a6cef494a2d17576b
tonho991 <67771574+tonho991@users.noreply.github.com>
2023-10-18 16:51:50 -0400

    181023

--- script/main.js
+++ script/main.js
@@ -1,5 +1,20 @@
 
+if(checkDialogClosed()){
+    document.getElementById('dialog').style.display = 'none';
+    document.getElementById('body-container').style.filter = "blur(0px)";
+}
+
 function closeDialog(){
     document.getElementById('dialog').style.display = 'none';
     document.getElementById('body-container').style.filter = "blur(0px)"
+    setCookie();
+}
+
+function checkDialogClosed(){
+    return document.cookie.indexOf('closed=true') !== -1;
+}
+
+function setCookie(){
+  var expires = new Date().getTime() + 3600;
+  document.cookie = "closed=true; expires=" + expires;
 }
