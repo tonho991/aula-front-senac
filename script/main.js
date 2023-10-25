73362b5318d067556808106989960a3a538c31e1
tonho991 <tonhoteca@gmail.com>
2023-10-25 15:57:42 -0400

    251023

--- script/main.js
+++ script/main.js
@@ -1,20 +1,16 @@
+function showMenuDown() {
+    var menu  = document.getElementById("menu");
+    var toolbar = document.getElementById("item1");
+    var hamburger = document.getElementById("icon-hamburger");
 
-if(checkDialogClosed()){
-    document.getElementById('dialog').style.display = 'none';
-    document.getElementById('body-container').style.filter = "blur(0px)";
-}
-
-function closeDialog(){
-    document.getElementById('dialog').style.display = 'none';
-    document.getElementById('body-container').style.filter = "blur(0px)"
-    setCookie();
-}
-
-function checkDialogClosed(){
-    return document.cookie.indexOf('closed=true') !== -1;
-}
-
-function setCookie(){
-  var expires = new Date().getTime() + 3600;
-  document.cookie = "closed=true; expires=" + expires;
-}
+    if (menu.className === "menu") {
+        menu.className += " responsive";
+        toolbar.className += " responsive";
+        hamburger.textContent="close"
+        
+    } else {
+        menu.className = "menu";
+        toolbar.className = "toolbar"
+        hamburger.textContent="menu"
+    }
+  }
