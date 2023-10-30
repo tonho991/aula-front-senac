da85f05720b5aafcc0bce3f43959d3e2c0bd68ac
Marco Antonio <67771574+tonho991@users.noreply.github.com>
2023-10-30 15:55:28 -0400

    301023

--- script/main.js
+++ script/main.js
@@ -1,16 +1,30 @@
 function showMenuDown() {
-    var menu  = document.getElementById("menu");
+    var menu = document.getElementById("menu");
     var toolbar = document.getElementById("item1");
     var hamburger = document.getElementById("icon-hamburger");
 
     if (menu.className === "menu") {
         menu.className += " responsive";
         toolbar.className += " responsive";
-        hamburger.textContent="close"
-        
+        hamburger.textContent = "close"
+
     } else {
         menu.className = "menu";
         toolbar.className = "toolbar"
-        hamburger.textContent="menu"
+        hamburger.textContent = "menu"
     }
-  }
+}
+
+function fabMouseEnter() {
+    document.getElementById("fab").style.animation = "expandIn 0.3s ease-out forwards";
+    document.getElementById("fab-title").style.display = "block"
+}
+
+function fabMouseLeave() {
+    document.getElementById("fab").style.animation = "expandOut 0.3s ease-out forwards"
+    document.getElementById("fab-title").style.display = "none"
+}
+
+function fabClick() {
+    window.location.href = "/pedido.html"
+}
